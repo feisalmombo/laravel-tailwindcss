693c261203c01a0153f7ed7744b0d35cbc9c8259
feisalmombo <feisalmombo29@gmail.com>
2023-01-05 13:55:15 +0300

    WIP

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./resources/**/*.{blade.php, vue, js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
+
+    // path/to/template/files
+    // './resources/views/welcome.blade.php',
+    // './resources/views/about.blade.php',
+    // './resources/views/*.blade.php',
+    // './resources/views/**/*.blade.php',
+    // './resources/views/**/*.{blade.php, vue, js}',
